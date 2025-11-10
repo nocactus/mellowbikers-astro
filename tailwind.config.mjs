52f2d0fa7d6562a1a093d982c11e265647e47058
Timo <timo@nocactus.nl>
2025-11-10 11:59:38 +0100

    Tailwind update Inter font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,1 +1,21 @@
 export default { content:["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"], theme:{ extend:{ colors:{ mellow:{ pink:'#ea4f86', dark:'#111827' }}}}, plugins:[]};
+
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        mellow: {
+          pink: '#ea4f86',
+          dark: '#111827',
+        },
+      },
+      fontFamily: {
+        sans: ['"Inter"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
